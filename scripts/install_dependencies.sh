07b77c039166829d34ed528e077336d4636101c2
bawkar-saurabh <40490386+bawkar-saurabh@users.noreply.github.com>
2020-10-17 11:59:36 +0530

    Add files via upload

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install apache2 php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y 
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install apache2 mysql-server php -y
